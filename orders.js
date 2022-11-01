832c0ad06e2edf5de291649fcf797d101b141d36
shrey <79036238+shreyzeous21@users.noreply.github.com>
2022-11-02 02:21:11 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+{
+    projectName: 'Downloader',
+    projectNumber: '1',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Joke Generator',
+    projectNumber: '2',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Quotes generator',
+    projectNumber: '3',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'QR Code Generator',
+    projectNumber: '4',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Snake Game',
+    projectNumber: '5',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'CALC',
+    projectNumber: '6',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Mood Generator',
+    projectNumber: '7',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Uncrack Password Generator',
+    projectNumber: '8',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Card Profile',
+    projectNumber: '9',
+    paymentStatus: 'none',
+    shipping: 'done',
+},
+{
+    projectName: 'Parallex Website',
+    projectNumber: '10',
+    paymentStatus: 'none',
+    shipping: 'done',
+}
+]
